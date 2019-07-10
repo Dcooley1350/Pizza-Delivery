11461f4f124d29c1cf401b7a66a1d2703ba4f24a
Devin Cooley <dcooley1350@gmail.com>
2019-07-09 22:32:09 -0700

    add user interface logic and constructor for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 // BUSINESS LOGIC
 
-function Pizza(size, toppings, style)
+function Pizza(size, toppings, style) {
+  this.size = size;
+  this.toppings = toppings;
+  this.style = style;
+}
 
 
 
@@ -13,6 +17,21 @@ function Pizza(size, toppings, style)
 
 // USER INTERFACE LOGIC
 
-
-var size = $("#size").val();
-var style = $("#style").val();
+$(document).ready(function(){
+  $("form#pizza-survey").submit(function(event) {
+    var size = $("#size").val();
+    var style = $("#style").val();;
+    $("input:checkbox[name=meat-toppings]:checked").each(function(){
+      var meatToppings = $(this).val();
+      toppings.push(meatToppings);
+    });
+    $("input:checkbox[name=veggie-toppings]:checked").each(function(){
+      var veggieToppings = $(this).val();
+      toppings.push(veggieToppings);
+    });
+    return (size,style)
+  });
+});
+console.log(toppings)
+console.log(size)
+console.log(style)
